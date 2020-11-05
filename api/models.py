65350bbb896d026940313932a5163e8296ae2c5c
Артур Гумеров <arturgumerov@yandex.ru>
2020-11-05 21:37:05 +0300

    For models merge

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    name
+    slug
+    pass
+
+
+class Genres(models.Model):
+    name
+    slug
+    pass
+
+
+class Titles(models.Model):
+    cathegory
+    genre
+    name
+    year
+    pass
