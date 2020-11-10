71fc650f3c5e6bbb9552113ff21b8c41a47c719f
Артур Гумеров <arturgumerov@yandex.ru>
2020-11-10 16:14:09 +0300

    for merge

--- users/views.py
+++ users/views.py
@@ -58,7 +58,7 @@ def user_token_view(request):
 
 class UserViewSet(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
     # def get_queryset(self):
     #     queryset = User.objects.all()
 
